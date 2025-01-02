238b93e35eae617b580ebdad68fdd6939f7ab8d1
ccb357 <134834835+ccb357@users.noreply.github.com>
2025-01-02 17:16:03 +0000

    reached to locales,you CAN run it but just know you have to do the rest yourself.

--- linux-utils/arch-distros/arch-install-uefi.sh
+++ linux-utils/arch-distros/arch-install-uefi.sh
@@ -24,3 +24,18 @@ mount /dev/sda3 /mnt
 
 # using pacstrap to install stuff.
 pacstrap -K /mnt base linux linux-firmware base-devel 
+
+#genfstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+#going into the installed os
+arch-chroot /mnt
+
+echo i cant do the timedate,so we are skipping that,if you want to make it, please run (ln -sf /usr/share/zoneinfo/Region/City /etc/localtime)
+
+# locales
+nano /etc/locale.gen
+
+locale-gen
+
+echo unfinished

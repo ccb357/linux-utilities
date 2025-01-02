61b6f13cc647364bc0c76305eb9f299e59133eb4
ccb357 <134834835+ccb357@users.noreply.github.com>
2025-01-02 06:38:09 +0000

    added archinstall
    DO NOT RUN THIS,IT IS UNFINISHED.

---
+++ linux-utils/arch-distros/arch-install-uefi.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#partitioning disks
+
+cfdisk
+
+
+#formatting disks
+
+mkdir /mnt/boot/efi
+
+mkfs.fat -32 /dev/sda1
+
+mkswap /dev/sda2
+
+mkfs.ext4 /dev/sda3
+
+#mounting partitions
+mount /dev/sda1 /mnt/boot/efi
+
+swapon /dev/sda2
+
+mount /dev/sda3 /mnt
+
+echo do the rest,this is unfinished.

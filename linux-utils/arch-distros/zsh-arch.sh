a2afa05ec949b7e93072134622bb1fb77fbc96cf
ccb357 <134834835+ccb357@users.noreply.github.com>
2025-01-02 11:47:27 +0000

    zsh for arch

---
+++ linux-utils/arch-distros/zsh-arch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+#Installing zsh
+
+
+sudo pacman -S zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting
+
+
+#installing ohmyZSH
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
